a76f36694b5ef9661aeb217e11b86a39dd5c2f80
Sean O'Daniels <sodaniels@gmail.com>
2024-01-03 13:32:24 -0500

    Solution for exercises 6.6 - 6.8

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import Anecdotes from './components/Anecdotes'
-import NewAnecdote from './components/NewAnecdote'
+import AnecdoteList from './components/AnecdoteList'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      <Anecdotes />
-      <NewAnecdote />
+      <AnecdoteList />
+      <AnecdoteForm />
     </div>
   )
 }
