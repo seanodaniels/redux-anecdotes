0039f01d99b674de60a5a29ef90efb8761b9ee2a
Sean O'Daniels <sodaniels@gmail.com>
2024-01-05 12:11:53 -0500

    Solutions for exercises 6.11 and 6.12. Notification functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteFilter from './components/Filter'
+import Notification from './components/Notification'
 import './App.css'
 
 const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
+      <Notification />
       <AnecdoteFilter />
       <AnecdoteList />
       <AnecdoteForm />
--- src/main.jsx
+++ src/main.jsx
@@ -8,4 +8,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
   </Provider>
-)
+) 
